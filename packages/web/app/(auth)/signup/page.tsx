442cf49bbd66479e631b6f7d3db05d8cf85e0780
shuhei <shuhei.nishikawa_111@icloud.com>
2025-12-22 00:16:19 +0900

    fix: useSearchParamの前にfallbackしてなかったエラーの対策をした

--- packages/web/app/(auth)/signup/page.tsx
+++ packages/web/app/(auth)/signup/page.tsx
@@ -1,13 +1,16 @@
+import { Suspense } from "react"
 import { MockRegistA, MockRegistB, MockRegistC } from "./mock"
 import { SignUpScreen } from "./SignUpScreen"
 
 export default function Page() {
   return (
     <>
+    <Suspense fallback={null}>
       <SignUpScreen />
       {/* <MockRegistA />
       <MockRegistB />
       <MockRegistC /> */}
+    </Suspense>
     </>
 )
 }
